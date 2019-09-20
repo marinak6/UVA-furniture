4dfaad43094c254c2222e049d038ed5dede1d32f
Bryan Tran <bt2kg@virginia.edu>
2019-09-20 12:35:19 -0400

    fixed dockercompose

--- microservices/models/views.py
+++ microservices/models/views.py
@@ -15,32 +15,32 @@ def createBid(request):
 @csrf_exempt
 def createFurniture(request):
 
-    # if request.method == "POST":
-    received_json_data = json.loads(request.body.decode("utf-8"))
+    if request.method == "POST":
+        received_json_data = json.loads(request.body.decode("utf-8"))
 
-    name = received_json_data["name"]
-    # bid_id = received_json_data['current_bid_id']
-    seller_id = received_json_data['seller']
-    is_bought = received_json_data['is_bought']
-    category_name = received_json_data['category']
-    # buyer = received_json_data["buyer"]
-    description = received_json_data["description"]
-    price = received_json_data["price"]
+        name = received_json_data["name"]
+        seller_id = received_json_data['seller']
+        is_bought = received_json_data['is_bought']
+        category_name = received_json_data['category']
+        # buyer = received_json_data["buyer"]
+        description = received_json_data["description"]
+        price = received_json_data["price"]
 
-    # current_bid_id = Bid.objects.get(id=bid_id)
-    seller = Person.objects.get(id=seller_id)
-    category = Category.objects.get(category=category_name)
+        seller = Person.objects.get(id=seller_id)
+        category = Category.objects.get(category=category_name)
 
-    obj = Furniture.objects.create(
-        name=name,
-        current_bid_id=None,
-        seller=seller,
-        is_bought=is_bought,
-        buyer=None,
-        description=description,
-        price=price,
-    )
-    obj.category.add(category)
-    obj.save()
-    obj_dict = model_to_dict(obj)
-    return JsonResponse({"Status": "Sucess"})
+        obj = Furniture.objects.create(
+            name=name,
+            current_bid_id=None,
+            seller=seller,
+            is_bought=is_bought,
+            buyer=None,
+            description=description,
+            price=price,
+        )
+        obj.category.add(category)
+        obj.save()
+        obj_dict = model_to_dict(obj)
+        return JsonResponse({"Status": "Sucess"})
+    else:
+        return JsonResponse({"Status": "Something went wrong"})
