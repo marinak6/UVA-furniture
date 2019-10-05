77c7f16215982406dbc68cd824ee0d41e987d5a6
Marina Kun <mk3qa@virginia.edu>
2019-10-05 14:46:09 -0400

    Made requests to get item specified in url

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -26,5 +26,8 @@ def home(request):
     return JsonResponse(resp)
 
 
-def item_details(request):
-    return render(request, 'item_details.html')
+def item_details(request, item_id):
+    req = urllib.request.Request('http://exp:8000/api/v1/item/'+str(item_id))
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    return render(request, 'item_details.html', {'item': resp})
