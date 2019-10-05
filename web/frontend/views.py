c1c54aa0c5cc64c6fbbdd42059be385caac4f7e8
Bryan Tran <bt2kg@virginia.edu>
2019-10-05 16:49:50 -0400

    stock photos

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -11,16 +11,9 @@ from django.shortcuts import (get_object_or_404, redirect, render,
 
 
 def home(request):
-
-    # req_url = 'http://exp:8000/api/v1/'
-
-    # resp_json = urllib.request.urlopen(req_url).read().decode('utf-8')
-    # resp = json.loads(resp_json)
-
     req = urllib.request.Request('http://exp:8000/api/v1/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp = json.loads(resp_json)
-
     # return JsonResponse(resp)
-    # return render(request, 'home.html')
-    return JsonResponse(resp)
+
+    return render(request, 'home.html', {'resp': resp["Res"]})
